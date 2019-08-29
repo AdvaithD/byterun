8054590be2211cf8acd046e0f51257c2c9f313dc
Advaith Doosa <advaithseo@gmail.com>
2019-08-28 22:33:08 -0700

    init

---
+++ Interpreter.py
@@ -0,0 +1,35 @@
+# Interpreter object has a stack
+# The list is initiated as a list
+
+
+class Interpreter:
+    def __init__(self):
+        self.stack = []
+    # push value onto a stack
+
+    def LOAD_VALUE(self, number):
+        self.stack.append(number)
+
+    def PRINT_ANSWER(self):
+        answer = self.stack.pop()
+        print(answer)
+
+    def ADD_TWO_VALUES(self):
+        first_num = self.stack.pop()
+        second_num = self.stack.pop()
+        total = first_num + second_num
+        self.stack.append(total)
+
+    # what_to_execute is a dictionary
+    def run_code(self, what_to_execute):
+        instructions = what_to_execute["instructions"]
+        numbers = what_to_execute["numbers"]
+        for each_step in instructions:
+            instruction, argument = each_step
+            if instruction == "LOAD_VALUE":
+                number = numbers[argument]
+                self.LOAD_VALUE(number)
+            elif instruction == "ADD_TWO_VALUES":
+                self.ADD_TWO_VALUES()
+            elif instruction == "PRINT_ANSWER":
+                self.PRINT_ANSWER()
